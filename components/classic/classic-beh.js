de50c2d85c8f6ecb46acf8aedbfbc727e1afeca3
CHN777 <unknown@example.com>
2018-12-27 11:25:12 +0800

    新增music、essay组价，使用behaviors来创建公共data，方便继承处理

---
+++ components/classic/classic-beh.js
@@ -0,0 +1,14 @@
+let classicBeh = Behavior({
+    behaviors: [],
+    properties: {
+        img: String,
+        content: String
+    },
+    data: {
+    },
+    attached() {},
+    methods: {
+    }
+});
+
+export {classicBeh}
